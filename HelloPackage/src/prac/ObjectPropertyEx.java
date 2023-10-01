c00e44ca7b8320da4c0e8cc03c8c352d73fdb705
LG <LG@DESKTOP-C576NK8>
2023-10-01 16:23:14 +0900

    231001 실습2

--- HelloPackage/src/prac/ObjectPropertyEx.java
+++ HelloPackage/src/prac/ObjectPropertyEx.java
@@ -43,13 +43,13 @@ public class ObjectPropertyEx {
 		Point b = new Point(2,3);
 		Point c = new Point(3,4);
 		
-		if (a == b) {
+		if (a == b) {        // false
 			System.out.println("a == b");
 		}
-		if (a.equals(b)) {
+		if (a.equals(b)) {        // true
 			System.out.println("a is equal to b");
 		}
-		if (a.equals(c)) {
+		if (a.equals(c)) {        // false
 			System.out.println("a is equal to c");
 		}
 	}
