ebf9836ce578e2a4cf953e0be607861584087abd
user <user@DESKTOP-DU01E7K>
2023-11-10 11:52:58 +0900

    231110 실습2

---
+++ Week10/src/gui/MyFrame.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+	public MyFrame() {
+		setTitle("First Frame");
+		setSize(300, 300);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyFrame frame = new MyFrame();
+	}
+}
